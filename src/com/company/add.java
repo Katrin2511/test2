ccaf74b20157f5a6687ffb245e592b472a8bbf68
Katrin2511 <shamina.ekaterina@yandex.ru>
2020-04-22 12:20:42 +0300

    1

---
+++ src/com/company/add.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class add {
+
+    public static void main(String[] args) {
+        System.out.println("Введите число");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        {
+            for (int i = 0; i < x; i++) {
+                System.out.print("Aплана ");
+            }
+        }
+    }
+}
